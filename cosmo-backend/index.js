9b76ee71ac736fec11d8657337599fc479e118c8
ChoncholKumarRay <fusitivechonchol@gmail.com>
2025-01-08 12:28:36 +0600

    setting up backend index file and admin router

---
+++ cosmo-backend/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken'); 
+const adminRoutes = require('./routes/adminRoutes');
+
+dotenv.config(); 
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); 
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+// app.use((req, res, next) => {
+//   console.log(`${req.method} ${req.url}`);
+//   next();
+// });
+  
+
+// Admin route
+app.use('/api/admin', adminRoutes); 
+
+
+//User routes for different functionality
+app.use('/api/user', userRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ cosmo-backend/routes/adminRoutes.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const router = express.Router();
+
+dotenv.config();
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  console.log(req.body);
+
+  if (username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ message: 'Login successful', token });
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' });
+  }
+});
+
+// Middleware to verify JWT token
+const authenticateJWT = (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) {
+    return res.status(403).json({ message: "Access Denied" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+// Admin route to get all bank users
+// router.get('/get-supply', authenticateJWT, async (req, res) => {
+//   try {
+//     const users = await User.find({}, 'username bank_account balance'); 
+//     res.json(users);
+//   } catch (error) {
+//     res.status(500).json({ message: 'Failed to fetch users' });
+//   }
+// });
+
+// module.exports = router;
